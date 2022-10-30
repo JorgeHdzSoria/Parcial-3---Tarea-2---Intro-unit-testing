72fb0845ec4860e8b10f1ebab3c76ce2782c3dd2
JorgeHdzSoria <jorge.hdzsoria@gmail.com>
2022-10-29 19:58:38 -0500

    Parcial 3 - Tarea 2 - Intro unit testing

---
+++ test/objetos/parcial3/intro/RectangleTest.java
@@ -0,0 +1,17 @@
+package objetos.parcial3.intro;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class RectangleTest {
+
+    @Test
+    public void testArea() {
+        Rectangle rectangle = new Rectangle(8, 4);
+        double area = rectangle.getArea();
+        double perimeter = rectangle.getPerimeter();
+
+        Assertions.assertEquals(32, area);
+        Assertions.assertEquals(24, perimeter);
+    }
+}
